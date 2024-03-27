18142a0a0c406d1d8560f514d4fbe2211479ea1b
aisav <aisavikin@gmail.com>
2024-03-27 15:26:55 +0300

    Пароли теперь записываются!

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ from controls import *
 class PasswordKeeper(ft.UserControl):
     def __init__(self):
         super().__init__()
-        choose = {
+        self.choose = {
             "kirill_low": False,
             "kirill_up": False,
             "latin_low": True,
@@ -15,15 +15,23 @@ class PasswordKeeper(ft.UserControl):
 
         self.name = ft.TextField(label="Введите название пароля", width=200)
         self.length = ft.TextField(label="Введите длину пароля", input_filter=ft.NumbersOnlyInputFilter(), width=200,
-                                    value="10")
-        self.generateButton = GeneratePasswordButton(choose, self.name, self.length)
+                                    value="10", on_change=self.check_length)
+        self.generateButton = GeneratePasswordButton(self.choose, self.name, self.length)
         self.showButton = ShowPasswordButton()
         self.createCodeButton = CreateCodeButton()
         self.delCodeButton = DelCodeButton()
-        self.checkBox = SelectSymbols(choose)
+        self.checkBox = SelectSymbols(self.choose)
 
     def build(self):
         password_button_row = ft.Row(controls=[self.generateButton, self.showButton], alignment=ft.alignment.center)
         code_button_row = ft.Row(controls=[self.createCodeButton, self.delCodeButton], alignment=ft.alignment.center)
         return ft.Column(controls=[password_button_row, code_button_row, self.name, self.length, self.checkBox],
                          alignment=ft.alignment.center)
+
+    def check_length(self, e):
+        if self.length.value == '':
+            self.length.value = 1
+        min_length = len([i for i in self.choose.values() if i])
+        if int(self.length.value) < min_length:
+            self.length.value = min_length
+        self.update()
